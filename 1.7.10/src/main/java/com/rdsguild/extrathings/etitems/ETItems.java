32905a8f7a73b3973d6787e3be55a93b669962bc
Armed Mob <armedmob@rdsguild.com>
2016-12-25 13:49:49 -0600

    Fund Commit
    Adding some basic things to make everything work

---
+++ 1.7.10/src/main/java/com/rdsguild/extrathings/etitems/ETItems.java
@@ -0,0 +1,7 @@
+package com.rdsguild.extrathings.etitems;
+
+/**
+ * Created by Armed Mob on 12/25/2016.
+ */
+public class ETItems {
+}
---
+++ 1.7.10/src/main/java/com/rdsguild/extrathings/etproxies/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.rdsguild.extrathings.etproxies;
+
+/**
+ * Created by Armed Mob on 12/25/2016.
+ */
+public class ClientProxy {
+}
